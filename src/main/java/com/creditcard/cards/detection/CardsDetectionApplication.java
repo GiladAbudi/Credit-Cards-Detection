1701a94e1f0847ed7910ab049526315646703162
Gilad <abudig@post.bgu.ac.il>
2022-04-20 12:45:23 +0300

    Initial commit

---
+++ src/main/java/com/creditcard/cards/detection/CardsDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.creditcard.cards.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditcard/cards/detection/CardsDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcard.cards.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
